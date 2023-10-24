4ad23b775cab04fd1a020e90661f324215393ab6
romulooaskes <romulocesaroaskes@gmail.com>
2023-10-24 15:09:16 -0300

    comentado

---
+++ aula15/vetornatela.js
@@ -0,0 +1,23 @@
+let valores = [1, 7, 5, 8, 9, 6]
+
+//console.log(valores)
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+console.log(valores[5])
+
+for(let rm in valores) {
+    console.log(`A posiçãoo ${rm} tem o valor ${valores[rm]}`)
+}
+*/
+for(let rm=0; rm < valores.length; rm++) {
+    console.log(`A posição ${rm} tem o valor ${valores[rm]}`)}
+    
+
+    
+
+    
+    
